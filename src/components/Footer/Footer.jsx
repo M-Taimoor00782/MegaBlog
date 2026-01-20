9a1e3ab2d5bf8878664ab83d5c9f609716029835
Muhammad Taimoor <mtaimoorjamali82@gmail.com>
2026-01-21 02:00:42 +0500

    new changes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import { Link } from "react-router-dom";
-import Logo from "../logo";
+import Logo from "../Logo";
 import { motion } from "framer-motion";
 import {
   FaFacebook,
